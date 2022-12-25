d0f03dedfb12922effa1f2ff07385f767df5b2a7
Oleg Kochiev <developer-artisan@mail.ru>
2022-12-25 19:17:00 +0300

    added main styles for header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import {homePageDatas} from '../../data/home';
 import {MyName} from '../../pages';
 import NavLinks from '../Home/NavLinks';
+import ThemeSwitcher from '../ThemeSwitcher';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px 15px;
+`;
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <MyName>{homePageDatas.myName}</MyName>
       <NavLinks />
-    </div>
+      <ThemeSwitcher />
+    </Container>
   );
 };
 
