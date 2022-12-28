4cc661ae5b0310e39a826ce51c34e7940d1a13d7
Oleg Kochiev <developer-artisan@mail.ru>
2022-12-28 18:07:38 +0300

    added links for portfolio tags

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,9 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/Header';
 import PageContainer from '../../components/PageContainer';
+import {PATHS} from '../../constants';
 
-const portfolioDatas = [
+const projects = [
+  {
+    tags: ['react', 'next.js'],
+    title: 'Создание собственного сайта портфолио',
+    data: '01.01.2023',
+    stackTechnologies: [],
+  },
+  {
+    tags: ['react', 'next.js'],
+    title: 'Создание собственного сайта портфолио',
+    data: '01.01.2023',
+    stackTechnologies: [],
+  },
   {
     tags: ['react', 'next.js'],
     title: 'Создание собственного сайта портфолио',
@@ -14,16 +28,41 @@ const portfolioDatas = [
 
 const Main = styled.main``;
 const Section = styled.section``;
-const Article = styled.article``;
+const Article = styled.article`
+  display: grid;
+  grid-template-columns: 120px 550px;
+  grid-gap: 30px;
+`;
+const Tags = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+const Tag = styled.div`
+  font-size: 14px;
+`;
+const Title = styled.div``;
+const Description = styled.div``;
 
 const Portfolio = () => {
   return (
     <PageContainer>
       <Header />
       <Main>
-        <Section>
-          <Article></Article>
-        </Section>
+        {projects.map((project) => (
+          <Section>
+            <Article>
+              <Tags>
+                {project.tags.map((tag) => (
+                  <Link href={PATHS.PORTFOLIO.BY_TAG(tag)}>
+                    <Tag>#{tag}</Tag>
+                  </Link>
+                ))}
+              </Tags>
+              <Title></Title>
+              <Description></Description>
+            </Article>
+          </Section>
+        ))}
       </Main>
     </PageContainer>
   );
---
+++ src/pages/portfolio/tag/[tag]/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostsByTag = () => {
+  return <div></div>;
+};
+
+export default PostsByTag;
