85c1a195e107220580a20db69cacdf4bb28c4ec4
Oleg Kochiev <developer-artisan@mail.ru>
2023-01-12 19:59:54 +0300

    chenged header display: flex -> grid

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,26 +3,50 @@ import React from 'react';
 import styled from 'styled-components';
 import {PATHS} from '../../constants';
 import {homePageDatas} from '../../data/home';
-import {MyName} from '../../pages';
 import NavLinks from '../Home/NavLinks';
 import ThemeSwitcher from '../ThemeSwitcher';
 
-const Container = styled.div`
+const HeaderContainer = styled.header`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 170px 550px;
+  grid-gap: 30px;
+  align-content: center;
   margin-bottom: 30px;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+`;
+
+const Logo = styled.div`
+  margin-left: auto;
+  padding-bottom: 5px;
+  font-size: 22px;
+  font-weight: bold;
 `;
 
 const Header = () => {
   return (
-    <Container>
-      <Link href={PATHS.HOME()}>
-        <MyName>{homePageDatas.myName}</MyName>
-      </Link>
-      <NavLinks />
-      <ThemeSwitcher />
-    </Container>
+    <HeaderContainer>
+      <GridContainer>
+        <Logo>
+          <Link href={PATHS.HOME()}>{homePageDatas.myName}</Link>
+        </Logo>
+        <NavContainer>
+          <NavLinks />
+          <ThemeSwitcher />
+        </NavContainer>
+      </GridContainer>
+    </HeaderContainer>
   );
 };
 
