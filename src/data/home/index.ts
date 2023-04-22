b77b77e0418340d447ab4bf42c1479c57ff4277f
Oleg Kochiev <developer-artisan@mail.ru>
2023-04-22 14:21:01 +0300

    added some changes

--- src/data/home/index.ts
+++ src/data/home/index.ts
@@ -8,7 +8,7 @@ export const socialIconsDatas = [
   {
     name: 'linkedIn',
     href: 'https://www.linkedin.com/in/oleg-kochiev/',
-    imgSrc: '/linkedin.svg',
+    imgSrc: '/linkedin2.svg',
     bgColor: '#0a66c2',
   },
   {
