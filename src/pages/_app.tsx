6b540df4170015fead25f3a6d0a90390235f4090
Oleg Kochiev <developer-artisan@mail.ru>
2023-05-02 21:47:53 +0300

    added some changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({Component, pageProps}: AppProps) {
     <GlobalContextProvider>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>Олег Кочиев | Сайт Портфолио</title>
         <link type="image/x-icon" rel="icon" href="/favicon.ico" />
         <link type="image/png" sizes="16x16" rel="icon" href="/icons/favicon-16x16.png" />
