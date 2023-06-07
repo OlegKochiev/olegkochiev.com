6bb5ce97f4f928b2458ed286139db3dabd5aa690
Oleg Kochiev <developer-artisan@mail.ru>
2023-06-07 17:46:51 +0300

    added CRUD for education routes

--- src/app/api/education/route.ts
+++ src/app/api/education/route.ts
@@ -1,7 +1,7 @@
 import {getCourses, addCourse, updateCourse, deleteCourse} from '../../../services/db/education';
 import {NextRequest, NextResponse} from 'next/server';
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   try {
     const courses = await getCourses();
     return NextResponse.json(courses, {status: 200});
@@ -10,16 +10,32 @@ export async function GET(req: Request) {
   }
 }
 
+export async function POST(req: NextRequest) {
+  try {
+    const course = await req.json();
+    await addCourse(course);
+    return NextResponse.json('Success', {status: 200});
+  } catch (error) {
+    return NextResponse.json(error, {status: 500});
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const {id, course} = await req.json();
+    await updateCourse(id, course);
+    return NextResponse.json('Success', {status: 200});
+  } catch (error) {
+    return NextResponse.json(error, {status: 500});
+  }
+}
+
 export async function DELETE(req: NextRequest) {
   try {
-    const reqBody = await req.json();
-    console.log(reqBody);
-
-    // await deleteCourse(id);
-    return NextResponse.json(reqBody, {status: 200});
+    const {id} = await req.json();
+    await deleteCourse(id);
+    return NextResponse.json('Success', {status: 200});
   } catch (error) {
-    console.log(error);
-
     return NextResponse.json(error, {status: 500});
   }
 }
